df262cb3a8f6bda66c7b1da8021c1a48005872bb
diego <diegobravi97@gmail.com>
2022-12-29 16:08:32 -0300

    five commit

--- src/Components/brand/Brand.jsx
+++ src/Components/brand/Brand.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
+import { google, slack, atlassian, dropbox, shopify } from './imports';
 import './brand.css'
 
-const brand = () => {
-  return (
-    <div>brand</div>
-  )
-}
+const Brand = () => (
+  
+  <div className="gpt3__brand section__padding">
+    <div>
+     <img src={google} />
+    </div>
 
-export default brand
+    <div>
+      <img src={slack} />
+    </div>
+
+    <div>
+      <img src={atlassian} />
+    </div>
+
+    <div>
+      <img src={dropbox} />
+    </div>
+
+   < div>
+      <img src={shopify} />
+    </div>
+</div>
+  
+);
+
+export default Brand
--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -5,10 +5,10 @@ import ai from '../../assets/ai.png'
 
 const Header = () => {
   return (
-    <div className="gpt3__header__padding" id="home">
+    <div className="gpt3__header section__padding" id="home">
       <div className="gpt3__header-content">
         <h1 className='gradient__text'>Let's build something amazing with GPT-3 OpenAI</h1>
-        <p>Let’s Build Something amazing with GPT-3 OpenAI Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of. Your Email Address Get Started 1.6k+ 1,600 people requested access a visit in last 24 hours</p>
+        <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
 
         <div className="gpt3__header-content__input">
           <input type="email" placeholder='Your Email Adress' />
@@ -19,11 +19,11 @@ const Header = () => {
           <img src={people} alt="people"/>
           <p>1,600 people requested access a visit in last 24 hours</p>
         </div>
-        <div className="gpt3__header-image">
-          <img src={ai} alt="ai"/>
-        </div>
       </div>
-
+      
+      <div className="gpt3__header-image">
+          <img src={ai}/>
+      </div>
     </div>
   )
 }
